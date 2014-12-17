9507e0f33ce0dff1575a1bcc7d16693e8fa0c5e3
KelKeltan <amiklsewicz@gmail.com>
2014-12-17 19:09:27 +0100

    v 1.0

---
+++ BLSTREAM PATRONAGE/AddPlacePage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace BLSTREAM_PATRONAGE
+{
+    
+    public sealed partial class AddPlacePage : Page
+    {
+        Place Filler = new Place();
+        public AddPlacePage()
+        {
+         
+           
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+         
+        }
+                  
+        
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Filler.Name = "Janusz";
+            Filler.Address = "Wielkoduszna 12/2";
+            Filler.Lattitude = 120;
+            Filler.Longitude = 130;
+            Filler.HasWifi = true;
+            
+            var LatToString = System.Convert.ToString(Filler.Lattitude);
+            Name_Text.Text = Filler.Name;
+            Address.Text = Filler.Address;
+            Lattitude.Text = LatToString;
+
+           
+        }
+
+        private async void Location_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Geolocator geolocator = new Geolocator();
+            geolocator.DesiredAccuracyInMeters = 50;
+
+                Geoposition geoposition = await geolocator.GetGeopositionAsync(
+                    maximumAge: TimeSpan.FromMinutes(5),
+                    timeout: TimeSpan.FromSeconds(10)
+                    );
+
+                Lattitude.Text = geoposition.Coordinate.Latitude.ToString("0.00");
+                Longitude.Text = geoposition.Coordinate.Longitude.ToString("0.00");
+            
+      
+        }
+    }
+}
---
+++ BLSTREAM PATRONAGE/Place.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLSTREAM_PATRONAGE
+{
+    interface INotifyPropertyChanged
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+        double Lattitude { get; set; }
+        double Longitude { get; set; }
+        bool HasWifi { get; set; }
+
+    }
+    class Place : INotifyPropertyChanged
+    {
+        private string _Name;
+        private string _Address;
+        private double _Lattitude;
+        private double _Longitude;
+        private bool _HasWifi;
+
+       /* public Place(string Name, string Address, double Lattitude, double Longitude, bool HasWifi)
+        {
+            _Name = Name;
+            _Address = Address;
+            _Lattitude = Lattitude;
+            _Longitude = Longitude;
+            _HasWifi = HasWifi;
+        }*/
+
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return _Address;
+            }
+            set
+            {
+                _Address = value;
+            }
+        }
+        public double Lattitude
+        {
+            get
+            {
+                return _Lattitude;
+            }
+            set
+            {
+                _Lattitude = value;
+            }
+        }
+        public double Longitude
+        {
+            get
+            {
+                return _Longitude;
+            }
+            set
+            {
+                _Longitude = value;
+            }
+        }
+        public bool HasWifi
+        {
+            get
+            {
+                return _HasWifi;
+            }
+            set
+            {
+                _HasWifi = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return (System.String.Format("{0} + {1}",Name,Address));
+        }
+    }
+}
